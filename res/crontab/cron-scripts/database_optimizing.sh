8b3d0c8b48166415f9272d91419c57c6b076ebec
Ruturaj <ruturaj@gmail.com>
2015-06-05 11:22:06 +0530

    removed cpu cooldown check for testing

--- res/crontab/cron-scripts/database_optimizing.sh
+++ res/crontab/cron-scripts/database_optimizing.sh
@@ -5,10 +5,10 @@
 	. /data/.siyah/${PROFILE}.profile;
 
 	if [ "$cron_db_optimizing" == "on" ]; then
-		while [ ! `cat /proc/loadavg | cut -c1-4` \< "3.50" ]; do
-			echo "Waiting For CPU to cool down";
-			sleep 30;
-		done;
+		#while [ ! `cat /proc/loadavg | cut -c1-4` \< "3.50" ]; do
+		#	echo "Waiting For CPU to cool down";
+		#	sleep 30;
+		#done;
 
 		for i in `find /data -iname "*.db"`; do 
 			/system/xbin/sqlite3 $i 'VACUUM;';
--- res/crontab/cron-scripts/zipalign.sh
+++ res/crontab/cron-scripts/zipalign.sh
@@ -8,10 +8,10 @@ PROFILE=`cat /data/.siyah/.active.profile`;
 
 if [ "$cron_zipalign" == "on" ]; then
 
-	while [ ! `cat /proc/loadavg | cut -c1-4` \< "3.50" ]; do
-        	echo "Waiting For CPU to cool down";
-        	sleep 30;
-	done;
+	#while [ ! `cat /proc/loadavg | cut -c1-4` \< "3.50" ]; do
+        #	echo "Waiting For CPU to cool down";
+        #	sleep 30;
+	#done;
 
 	if [ `pgrep -f "zipalign" | wc -l` \< 5 ]; then
 		echo "Starting zipalign, it's will take 2min to finish, please wait.";
